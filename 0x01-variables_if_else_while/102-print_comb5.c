2b62ed9cf37718dde27eb3c1fd2be72f21a77688
bless643 <fawolebless1@hotmail.com>
2022-10-15 16:59:00 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints all combinations of two two digits with,
+ *main - Prints all combinations of two two digits with,
  * and space followed by new line
  *
- * Return: returns 0
+ *Return: returns 0
  */
 int main(void)
 {
@@ -28,4 +28,4 @@ int main(void)
 	}
 	putchar(10);
 	return (0);
-
+}
