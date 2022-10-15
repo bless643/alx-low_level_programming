5dd449623ed6f4cb40a731ea6beda52c0d978401
bless643 <fawolebless1@hotmail.com>
2022-10-15 12:24:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,24 +20,27 @@ int main(void)
 	g = 48;
 	while (g < 58)
 	{
-		if (e != i && e != g && i != g && e < i && i < g)
+	if (e != i && e != g && i != g && e < i && i < g)
 		{
 		       putchar(e);
 		       putchar(i);
 		       putchar(g);
-		       if (i == 56 && e == 55 && g == 57)
-		       {
-			       break;
-		       }
-		       putchar(',');
-		       putchar(' ');
-		}
-		g++;
-	}
-	i++;
+        if (i == 56 && e == 55 && g == 57)
+{	
+        break;
 }
+        putchar(',');
+       	putchar(' ');
+		
+	}
+		g++;
+		}
+i++;
+}
+
 e++;
 }
+
 putchar('\n');
 return (0);
 }
