7a36acdc82ebe3446aa8cf486cce776e71f3882f
bless643 <fawolebless1@hotmail.com>
2022-10-15 17:13:45 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,48 +1,33 @@
 # include <stdio.h>
 /**
- * main - Entry point digit numbers
- * Description : prints all possible different combinations of three digits
- * Return :0
+ *main - Prints all combinations of three digits with,
+ * and space followed by new line
+ *
+ *Return: returns 0
  */
 int main(void)
 {
-	int i, e, g;
+	int digit1, digit2, digit3;
 
-	i = 48;
-	e = 48;
-	g = 48;
-
-	while (e < 58)
-{
-	i = 48;
-	while (i < 58)
-{
-	g = 48;
-	while (g < 58)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-	if (e != i && e != g && i != g && e < i && i < g)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-		       putchar(e);
-		       putchar(i);
-		       putchar(g);
-        if (i == 56 && e == 55 && g == 57)
-{	
-        break;
-}
-        putchar(',');
-       	putchar(' ');
-		
-	}
-		g++;
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
 		}
-i++;
+	}
+	putchar('\n');
+	return (0);
 }
-
-e++;
-}
-
-putchar('\n');
-return (0);
-}
-
-
