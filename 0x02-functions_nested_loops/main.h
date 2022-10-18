a9aa2511ca65a20cf9f2b4b87a8337713bdade10
bless643 <fawolebless1@hotmail.com>
2022-10-18 13:46:54 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,20 @@
 
 /**
  * *filename:main.h
- * *Descrition : header file contaning function declarations
+ * *Descrition : header file contaning  function declarations
+ * *Author :blessing fawole
  */
-int _putchar(char c);
-void print_alphabet(void);
+
 void print_alphabet_x10(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table(void);
+void print_to_98(int n); 
+void times_table(void); 
+int _putchar(char);
 int _islower(int c);
 int_isalpha(int c);
 int print_sign(int);
 int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+int print_last_digit(int);                                                                                                           
