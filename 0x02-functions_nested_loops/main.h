489564181b0306fddecc85c4923a0f97862f5f3c
bless643 <fawolebless1@hotmail.com>
2022-10-18 13:52:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ void print_alphabet(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
 void times_table(void);
-void print_to_98(int n); 
+void print_to_98(int ); 
 void times_table(void); 
 int _putchar(char);
 int _islower(int c);
