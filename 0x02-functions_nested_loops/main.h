3aa22a0c16f0b1a0014b5480ab8e5b6005fe30d8
bless643 <fawolebless1@hotmail.com>
2022-10-18 13:33:29 -0700

    Main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,18 @@
-#include <unistd.h>
 
 /**
- * *_putchar - write the character c to stdout
- *
- * *@c: the character to print
- *
- * *Return 0n sucess 1
- *
- * * on error -1 is returned and errno is set appropriately.
+ * *filename:main.h
+ * *Descrition : header file contaning function declarations
  */
-
-int_putchar(char c;
-
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int_isalpha(int c);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
